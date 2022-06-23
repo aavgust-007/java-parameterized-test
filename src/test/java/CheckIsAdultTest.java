85b4aa5a9f592bf0f562c27c8b9954d77204410f
Avgustina Kashleva <avgust-007@yandex.com>
2022-06-23 21:00:02 +0600

    My parameterized test

---
+++ src/test/java/CheckIsAdultTest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class) // Напиши аннотацию для параметризованных тестов
+public class CheckIsAdultTest {
+
+    private final int age;
+    private final boolean result;
+
+    public CheckIsAdultTest(int age, boolean result) {
+        this.age = age;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTextData() {
+        return new Object[][] {
+                {18,true},
+                {19,true},
+                {16,false},
+                {21,true}
+                // Заполни массив тестовыми данными и ожидаемым результатом
+        };
+    }
+
+    @Test
+    public void checkIsAdultWhenAgeThenResult() {
+        Program program = new Program();
+        // Передай сюда возраст пользователя
+        boolean isAdult = program.checkIsAdult(age);
+        // Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+        assertEquals("Пользователь несовершеннолетний",result,isAdult);
+    }
+}
